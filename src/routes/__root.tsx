cb18aef26830ba1450a9b764bf15179d96a6e380
Brian Cooper <brian@brian-cooper.com>
2026-03-10 13:10:56 -0400

    chore: add tsc type check to pre-commit hook

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ import { createServerFn } from "@tanstack/react-start";
 import { DefaultCatchBoundary } from "@/components/layout";
 import { Toaster } from "@/components/ui/sonner";
 import app from "@/lib/config/app.config";
-import { fetchMaintenanceMode } from "@/lib/flags";
+import { fetchMaintenanceMode } from "@/lib/providers";
 import appCss from "@/lib/styles/globals.css?url";
 import createMetaTags from "@/lib/util/createMetaTags";
 import ThemeProvider from "@/providers/ThemeProvider";
